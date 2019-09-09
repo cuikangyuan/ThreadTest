de1bd849fc0b032cf90b0ea5feb14d777d61a53a
cuikangyuan <cuikangyuan@51tiangou.com>
2019-09-09 14:34:32 +0800

    add test code

---
+++ src/ReadWriteLockBegin1/ThreadA.java
@@ -0,0 +1,16 @@
+package ReadWriteLockBegin1;
+
+public class ThreadA extends Thread {
+
+
+    private Service service;
+
+    public ThreadA(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.read();
+    }
+}
---
+++ src/ReadWriteLockBegin2/ThreadB.java
@@ -0,0 +1,16 @@
+package ReadWriteLockBegin2;
+
+public class ThreadB extends Thread {
+
+
+    private Service service;
+
+    public ThreadB(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.write();
+    }
+}
